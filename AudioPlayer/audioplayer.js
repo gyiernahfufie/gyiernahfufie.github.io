404025e4696b1157b2ea82e06609f3b859ff7cf9
Mark <ugamarkj@yahoo.com>
2018-08-21 18:11:14 -0400

    attempt 2

--- AudioPlayer/audioplayer.js
+++ AudioPlayer/audioplayer.js
@@ -1,20 +1,23 @@
 'use strict';
 
+
 (function () {
-  $(document).ready(function () { //once page is ready
-  	var sound = new Howl({
-      src: ['Ansley.mp3']
-    });
-    playAudio()
-    $('#play').click(stopAudio());
 
-  });
+	var sound = new Howl({
+	  src: ['Ansley.mp3']
+	});
+	$(document).ready(function () { //once page is ready
+		
+		playAudio()
+		$('#play').click(stopAudio());
+
+	});
   
-
-  function playAudio() {
-    sound.play();
-  }
-  function stopAudio() {
-  	sound.stop();
-  }
+	function playAudio() {
+		sound.play();
+	}
+	
+	function stopAudio() {
+		sound.stop();
+	}
 })();
