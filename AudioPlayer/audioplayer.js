ac48fe106ab9590d259834ee1e040a60034f1a9e
Mark <ugamarkj@yahoo.com>
2018-08-21 18:59:11 -0400

    attempt 11

--- AudioPlayer/audioplayer.js
+++ AudioPlayer/audioplayer.js
@@ -8,18 +8,17 @@
 		  src: ['Ansley.mp3']
 		});
 		//playAudio();
+		$('#playButton').click(playAudio());
+		$('#stopButton').click(stopAudio());
 	});
-  
-  	//$('#playButton').click(playAudio());
-	//$('#stopButton').click(stopAudio());
 
 	function playAudio() {
 		console.log('play audio');
 		var soundID = sound.play();
-	}
+	};
 
 	function stopAudio() {
 		sound.stop();
 		console.log('stopping sound');
-	}
+	};
 })();
