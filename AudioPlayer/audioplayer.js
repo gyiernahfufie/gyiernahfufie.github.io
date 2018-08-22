1dbc502107f66edd31abb2168a01b7d8fa428308
PIEDMONT_NT\117120 <mark.jackson@piedmont.org>
2018-08-22 07:27:35 -0400

    parameters test

--- AudioPlayer/audioplayer.js
+++ AudioPlayer/audioplayer.js
@@ -4,6 +4,9 @@
 (function () {
 	var sound = '';
 	$(document).ready(function () { //once page is ready
+		var param = getJsonFromUrl();
+		console.log(param);
+		
 		sound = new Howl({
 		  src: ['Ansley.mp3']
 		});
@@ -15,10 +18,21 @@
 	function playAudio() {
 		console.log('play audio');
 		var soundID = sound.play();
-	};
+	}
 
 	function stopAudio() {
 		sound.stop();
 		console.log('stopping sound');
-	};
+	}
+
+	function getJsonFromUrl() {
+	  var query = location.search.substr(1);
+	  var result = {};
+	  query.split("&").forEach(function(part) {
+	    var item = part.split("=");
+	    result[item[0]] = decodeURIComponent(item[1]);
+	  });
+	  return result;
+	}
+
 })();
